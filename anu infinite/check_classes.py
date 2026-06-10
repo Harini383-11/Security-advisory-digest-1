4ce61f2c9ac04c510aa46dafa388e182eb4cf023
Anusre1987 <ramakrishnanm446@gmail.com>
2026-06-10 09:28:49 +0530

    Delete anu infinite directory

--- anu infinite/check_classes.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import inspect
-from src.modules import dedup, rag, llm_service, agent
-
-for m, name in [(dedup, 'dedup'), (rag, 'rag'), (llm_service, 'llm_service'), (agent, 'agent')]:
-    classes = [x for x in dir(m) if inspect.isclass(getattr(m, x)) and not x.startswith('_')]
-    print(f"{name}: {classes}")
